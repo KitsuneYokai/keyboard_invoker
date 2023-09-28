59b6a97a5f432731eef89fdc9b78f9eb66347efc
KitsuneYokai <56147653+KitsuneYokai@users.noreply.github.com>
2023-09-28 15:14:30 +0200

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 // import provider for the keyboard invoker
 import 'package:flutter/services.dart';
 import 'package:keyboard_invoker/keyboard_invoker.dart';
@@ -24,14 +23,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
   late FocusNode _focusNode;
   final macroRecordingScrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
     // initializing the focus node
     _focusNode = FocusNode();
   }
@@ -78,27 +75,6 @@ class _MyAppState extends State<MyApp> {
     LogicalKeyboardKey.keyR,
   ];
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = await _keyboardInvokerPlugin.getPlatformVersion() ??
-          'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final keyboardInvokerPlugin = Provider.of<KeyboardInvoker>(context);
@@ -121,9 +97,6 @@ class _MyAppState extends State<MyApp> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Center(
-                    child: Text('Running on: $_platformVersion\n'),
-                  ),
                   TextField(
                     focusNode: _focusNode,
                     decoration: const InputDecoration(
